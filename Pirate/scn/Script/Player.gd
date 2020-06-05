d5a9c10c070b98ebbf63ddc163a1d370046a3ea0
MrSheeplip <64995222+MrSheeplip@users.noreply.github.com>
2020-06-05 22:02:23 +0200

    add function RandPos Islands Chests

--- Pirate/scn/Script/Player.gd
+++ Pirate/scn/Script/Player.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
-func Hunger(playerPosTile, mousePosTile, distanceMax, preHungerLabel : Label, hunger):
+var Life : int 
+var Hunger : int = 20
+var Money : int 
+var bullet : int
+
+func movement(playerPosTile, mousePosTile, distanceMax, preHungerLabel : Label):
 	var DistancePlayerToMouse = playerPosTile.distance_to(mousePosTile) as int 
 	var PreHunger : int
 	var RedFont = Color(1, 0, 0, 1) 
@@ -11,7 +16,7 @@ func Hunger(playerPosTile, mousePosTile, distanceMax, preHungerLabel : Label, hu
 	elif DistancePlayerToMouse < distanceMax:
 		distanceMax = DistancePlayerToMouse - 1
 	
-	if hunger < PreHunger:
+	if Hunger < PreHunger:
 		preHungerLabel.self_modulate = "e12222"
 	else:
 		preHungerLabel.self_modulate = "ffffff"
